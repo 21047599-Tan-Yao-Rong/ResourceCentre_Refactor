da3c82324a562236b7d448033ac7ca0f8067b1ac
21016430 <21016430@10.174.115.209>
2022-07-22 13:13:10 +0800

    CF

--- ResourceCentre_Refactor/src/Camcorder.java
+++ ResourceCentre_Refactor/src/Camcorder.java
@@ -12,9 +12,10 @@ public class Camcorder extends Item{
 	}
 	
 	public String toString(){
-		String output = "";
+		String info = super.toString();
 		// Write your codes here
-		return output;
+		
+		return String.format("%-63s %-20d", info, opticalZoom);
 	}
 }
 
--- ResourceCentre_Refactor/src/Chromebook.java
+++ ResourceCentre_Refactor/src/Chromebook.java
@@ -12,9 +12,10 @@ public class Chromebook extends Item{
 	}
 	
 	public String toString(){
-		String output = "";
+		String info = super.toString();
 		// Write your codes here
-		return output;
+		
+		return String.format("%-63s %-20s",info, os);
 	}
 }
 
